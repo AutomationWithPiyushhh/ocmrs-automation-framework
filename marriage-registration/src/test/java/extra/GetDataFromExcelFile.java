6fb075ab35629d49c683c76ebe94a00413b4cb22
AutomationWithPiyush <automation.with.piyush@gmail.com>
2025-08-13 10:39:44 +0530

    commited

--- marriage-registration/src/test/java/extra/GetDataFromExcelFile.java
+++ marriage-registration/src/test/java/extra/GetDataFromExcelFile.java
@@ -13,27 +13,32 @@ import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 public class GetDataFromExcelFile {
 	public static void main(String[] args) throws EncryptedDocumentException, IOException {
-
-//		Get the java rep object of the physical file
-
 		FileInputStream fis = new FileInputStream(
 				"C:\\Users\\User\\git\\ocmrs\\marriage-registration\\src\\test\\resources\\testData.xlsx");
 
 		Workbook wb = WorkbookFactory.create(fis);
+//		Wife Photo	wife Address	Wife Religion	Wife DOB	Wife Marital Status	Wife Zipcode	Wife State	Wife Aadhaar		Witness 1 Name	Witness 1 Address		Witness 2 Name	Witness 2 Address		Witness 3 Name	Witness 3 Address
 
-		Sheet sh = wb.getSheet("matrimony");
+		String husName = wb.getSheet("matrimony").getRow(1).getCell(2).getStringCellValue();
+		String HusbandPhoto = wb.getSheet("matrimony").getRow(1).getCell(3).getStringCellValue();
+		String HusbandAddress = wb.getSheet("matrimony").getRow(1).getCell(4).getStringCellValue();
+		String HusbandReligion = wb.getSheet("matrimony").getRow(1).getCell(5).getStringCellValue();
+//		String HusbandDOB = wb.getSheet("matrimony").getRow(1).getCell(6).getStringCellValue();
+		String HusbandMaritalStatus = wb.getSheet("matrimony").getRow(1).getCell(7).getStringCellValue();
+//		String HusbandZipcode = wb.getSheet("matrimony").getRow(1).getCell(8).getStringCellValue();
+		String HusbandState = wb.getSheet("matrimony").getRow(1).getCell(9).getStringCellValue();
+//		String HusbandAadhaar = wb.getSheet("matrimony").getRow(1).getCell(10).getStringCellValue();
 		
-		Row row = sh.getRow(6);
+		String wifeName = wb.getSheet("matrimony").getRow(1).getCell(12).getStringCellValue();
+		String wifePhoto = wb.getSheet("matrimony").getRow(1).getCell(13).getStringCellValue();
+		String wifeAddress = wb.getSheet("matrimony").getRow(1).getCell(14).getStringCellValue();
+		String wifeReligion = wb.getSheet("matrimony").getRow(1).getCell(15).getStringCellValue();
+//		String wifeDOB = wb.getSheet("matrimony").getRow(1).getCell(16).getStringCellValue();
+		String wifeMaritalStatus = wb.getSheet("matrimony").getRow(1).getCell(17).getStringCellValue();
+//		String wifeZipcode = wb.getSheet("matrimony").getRow(1).getCell(18).getStringCellValue();
+		String wifeState = wb.getSheet("matrimony").getRow(1).getCell(19).getStringCellValue();
+//		String wifeAadhaar = wb.getSheet("matrimony").getRow(1).getCell(20).getStringCellValue();
 		
-		Cell cell = row.getCell(2);
-		
-		
-		 String value = cell.getStringCellValue();
-		 System.out.println(value);
-		
-		
-//		cell.getBooleanCellValue();
-//		cell.getNumericCellValue();
-//		cell.getLocalDateTimeCellValue();
+		System.out.println(HusbandAddress);
 	}
 }
